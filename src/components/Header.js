e281390d154feb83a8b11bab1238bd144f090082
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-07 14:57:39 +0200

    Add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import { NavLink } from "react-router-dom";
 
 export function Header(){
     return (
-        <div className="header">
-            <NavLink to="/evenements">Événements</NavLink>
+        <>
+        <nav>
+            <h1>Dessine-moi un bébé</h1>
             <NavLink to="/ateliers">Ateliers</NavLink>
             <NavLink to="/bibliotheque">Bibliothèque</NavLink>
             <NavLink to="/equipe">Qui sommes-nous ?</NavLink>
-        </div>
+        </nav>
+        </>
     );
 }
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,16 +1,12 @@
 import React, { useLayoutEffect } from 'react'
 import { NavLink , Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
 
 export function RootLayout(){
     return (
         <div className="root-layout">
             <header>
-                <nav>
-                    <h1>Dessine-moi un bébé</h1>
-                    <NavLink to="/ateliers">Ateliers</NavLink>
-                    <NavLink to="/bibliotheque">Bibliothèque</NavLink>
-                    <NavLink to="/equipe">Qui sommes-nous ?</NavLink>
-                </nav>
+                <Header />
             </header>
             <main>
                 <Outlet />
