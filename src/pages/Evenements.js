56eca43f62a948cca38fd9cbba89bd225985d678
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-07 10:54:58 +0200

    Add events

--- src/pages/Evenements.js
+++ src/pages/Evenements.js
@@ -1,9 +1,39 @@
-const evenements = {
-    id: 1,
-    theme: "Tente rouge : Féminité et parentalité",
-    date: "samedi 19 octobre",
-    lieu: "Marseille (Centre Fissiaux, 13004)",
-    nbPlaces: 5,
-    description: "Moment d’échange et de partage réservé aux femmes. Sous la tente, dans une ambiance intimiste, chacune prendra la parole et sera écoutée, dans le respect et le non-jugement.",
-    public: "Cette journée OUVERTE À TOUS rassemblera des associations, des institutions, des parents, des enfants, des psychologues, des ostéopathes, des médecins, des psychomotriciens, des orthophonistes, des éducateurs de jeunes enfants, des animateurs, des étudiants, des enseignants, des universitaires, des artistes...!"
-}
+const evenements = [
+    {
+        id: 1,
+        theme: "Tente rouge : Féminité et parentalité",
+        date: "samedi 19 octobre 2019",
+        horaire: "10h30 à 13h",
+        lieu: "Marseille (Centre Fissiaux, 13004)",
+        nbPlaces: 7,
+        contact: "amandine.dessinemoiunbebe@live.fr",
+        description: "Moment d’échange et de partage réservé aux femmes. Sous la tente, dans une ambiance intimiste, chacune prendra la parole et sera écoutée, dans le respect et le non-jugement.",
+        public: "Cette journée OUVERTE À TOUS rassemblera des associations, des institutions, des parents, des enfants, des psychologues, des ostéopathes, des médecins, des psychomotriciens, des orthophonistes, des éducateurs de jeunes enfants, des animateurs, des étudiants, des enseignants, des universitaires, des artistes...!",
+        tarif: "Boîte à don"
+    },
+    {
+        id: 2,
+        theme: "Tente rouge",
+        date: "samedi 20 septembre 2020",
+        horaire: "20h",
+        lieu: "Ventabren",
+        nbPlaces: 5,
+        contact: "contact@dessinemoiunbebe.fr",
+        description: "La tente rouge est un espace intimiste et bienveillant pour partager nos expériences de femmes : positives, singulières, douloureuses parfois, extra-ordinaires... un lieu pour déposer nos difficultés, raconter nos joies, être à l'écoute d'autres réflexions...",
+        public: "Réservé aux femmes",
+        tarif: "Boîte à don"
+    },
+    {
+        id: 3,
+        theme: "Contes et goûter",
+        date: "mardi 25 février 2021",
+        horaire: "17h",
+        lieu: "Chez Eléonore, à Lançon de Provence",
+        nbPlaces: 5,
+        contact: "auroretino@yahoo.fr",
+        description: "Un après-midi contes pour petits et grands. Après les contes goûter partagé tiré du sac. ",
+        public: "Pour petits et grands",
+        tarif: "Réservé aux adhérents (possibilité d'adhérer sur place 10€/famille)"
+    }
+    
+]
