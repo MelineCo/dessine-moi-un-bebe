bcf31c906f11fd50218bc66efb9e243fd3ed8974
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-08 22:57:32 +0200

    Style library

--- src/components/categories-livre/Books.js
+++ src/components/categories-livre/Books.js
@@ -4,14 +4,12 @@ export default function Books(){
     const books = useLoaderData()
     return (
         <div>
-            <h3>Livres nouveau test</h3>
-            <p>Don't push!</p>
+            <h3>Livres</h3>
 
-            <div className="book">
+            <div className="books">
                 {books?.map(book => (
                     <Link to={book.id.toString()} key={book.id} >
                         <p>{book.titre}</p>
-                        <p>{book.theme}</p>
                     </Link>
                 ))}
 		    </div>
--- src/layouts/BooksLayout.js
+++ src/layouts/BooksLayout.js
@@ -5,13 +5,15 @@ export default function BooksLayout(props){
     return (
         <div>
             <h2>Bibliotheque</h2>
-            <img className="illustration-bibli" src={illustration} alt="Femme lisant un livre sur la maternité"></img>
             <p>
                 Pour emprunter un livre (ou des livres), il convient d'etre adhérent à jour de sa cotisation
                 et de nous remettre un chèque de 40€ de caution non encaissé.<br />
                 Envoyez nous un mail pour récupérer votre ouvrage et nous conviendrons d'un rendez-vous: <a href="mailto:contact@dessinemoiunbebe.fr?subject=Emprunt livre">contact@dessinemoiunbebe.fr</a>
             </p>
-            <Outlet />
+            <div className="book-grid">
+                <Outlet />
+            </div>
+            
         </div>
     );
 }
