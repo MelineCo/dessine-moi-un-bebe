7a479e9331419f8eb281e9b68842cb9603c8d369
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-13 00:00:35 +0200

    Add homepage welcoming text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
+import illuMaternite from '../images/Motherhood-pana.svg';
+
 export function Home(){
     return (
-        <h1>Home</h1>
+        <div className='flex'>
+            <div className="hero">
+                <h1>Dessine-moi un bébé : l'association qui soutient les&nbsp;parents</h1>
+                <p>Nous sommes toujours là pour du soutien que ce soit sur l'allaitement, le portage, les couches lavables, un projet de naissance, un accouchement à la maison, la communication non violente avec son homme & les enfants, un besoin de papoter... ça peut se faire par téléphone, par mail, ou une rencontre dans un parc pourquoi pas !</p>
+                <button>J'adhère !</button>
+            </div>
+        
+            <img style={{width: 400}} src={illuMaternite} alt="Maternité" ></img>
+        </div>
     );
 }
